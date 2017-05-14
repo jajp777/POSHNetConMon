4e6ab1fff18a62b19fc244294c674dad1149b47f
windowsPosher <sthkppl@gmail.com>
2017-05-14 10:57:48 -0400

    Charting, Analytics
    Actually works, sort of

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # POSHNetConMon
-The goal of this project is to create a way to monitor network traffic from any Windows machine in an easy to use manner. 
-Without the installation or utilization of any external tools. This allows any Windows machine to become a connection monitor, 
-with an easily viewable GUI to monitor network connections which may or may not be malicious. On many networks and machines, 
-you are unable to install third-party tools or run third-party executables, all of which GUI based network monitors currently use. 
-By using Powershell we are able to bypass the usual restrictions placed on a Windows machine to see inbound and outbound traffic in an easy to read fashion.
+
+## Usage
+Run the script from an elevated PowerShell instance. Two parameters are require [-IP] and [-Seconds]. 
+IP address is the local IP address of your current machine and interface you would like to monitor on.
+Seconds is how long the tool will run and monitor before analyzing and displaying data.
+
+### Example
+
+```
+.\netConMon.ps1 -IP 192.168.1.1 -Seconds 10
--- netConMon.ps1
+++ netConMon.ps1
@@ -1,4 +1,20 @@
-﻿$Global:decodedPackets = [System.Collections.ArrayList]@()
+﻿param([string]$IP,[string]$seconds)
+
+# load the appropriate assemblies 
+[void][Reflection.Assembly]::LoadWithPartialName(“System.Windows.Forms”) 
+[void][Reflection.Assembly]::LoadWithPartialName(“System.Windows.Forms.DataVisualization")
+# Variable $ File Init
+$Global:decodedPackets = [System.Collections.ArrayList]@()
+$Script:packetCount = 0
+$Script:analyzedConnections = @{}
+$Script:serviceCounts = @{}
+$Script:protocolCounts = @{}
+$Script:timeData = @{}
+$Script:dataCounts = @{}
+$Script:localIP = $IP
+# Services                     
+$serviceFile = [IO.File]::ReadAllText("$env:windir\System32\drivers\etc\services") -split
+([Environment]::NewLine) -notlike "#*"
 
 Function getService( $port )
 {
@@ -18,7 +34,6 @@ Function getService( $port )
 		}
 	}
 }
-
 Function NetworkToHostUInt16( $address )
 {
 	[Array]::Reverse( $address )
@@ -35,13 +50,12 @@ Function ByteToString( $address )
 	return $AsciiEncoding.GetString( $address )
     }
 
-function capturePackets{
-    param( [String]$LocalIP = "NotSpecified", [String]$ScanIP="all", [String]$Protocol = "all", `
-		    [String]$Port="all", [Int]$Seconds = 0, [switch]$ResolveHosts, [switch]$Help )
+Function capturePackets{
+    param([int]$seconds=30 )
     
     $byteIn = New-Object Byte[] 4			# source
     $byteOut = New-Object Byte[] 4			# destination
-    $byteData = New-Object Byte[] 4096
+    $byteData = New-Object Byte[] 8192
 
     $byteIn[0] = 1  						# enable promiscuous mode
     $byteIn[1-3] = 0
@@ -51,9 +65,10 @@ function capturePackets{
     $Socket = New-Object System.Net.Sockets.Socket( [Net.Sockets.AddressFamily]::InterNetwork, [Net.Sockets.SocketType]::Raw, [Net.Sockets.ProtocolType]::IP )
     $Socket.SetSocketOption( "IP", "HeaderIncluded", $true )
     # Packet buffer
-    $Socket.ReceiveBufferSize = 1024000
+    #$Socket.ReceiveBufferSize = 1024000
+    $Socket.ReceiveBufferSize = 819200
     # Create IP endpoint
-    $Endpoint = New-Object System.Net.IPEndpoint( [Net.IPAddress]"$LocalIP", 0 )
+    $Endpoint = New-Object System.Net.IPEndpoint( [Net.IPAddress]"$localIP", 0 )
     $Socket.Bind( $Endpoint )
     # Promiscuous mode
     [void]$Socket.IOControl( [Net.Sockets.IOControlCode]::ReceiveAll, $byteIn, $byteOut )
@@ -62,28 +77,32 @@ function capturePackets{
     $packets = @()							# array for packets
     $running = $true
     $count = 1
-    while( $running )
+    $runTime = New-TimeSpan -Seconds $seconds
+    $sw = [diagnostics.stopwatch]::StartNew()
+    while($sw.elapsed -lt $runTime)
     {
+        Write-Progress -activity "Capturing Packets" -status "Time Left: " -SecondsRemaining($seconds - ($sw.ElapsedMilliseconds/1000))
         if( -not $Socket.Available )
 	    {
 		    start-sleep -milliseconds 300
 		    continue
 	    }
-    # receive data
-	    $rData = $Socket.Receive( $byteData, 0, $byteData.length, [Net.Sockets.SocketFlags]::None )
+        # receive data
+	    #$rData = $Socket.Receive( $byteData, 0, $byteData.length, [Net.Sockets.SocketFlags]::None )
+	    $rData = $Socket.Receive($byteData)
+        
         $packetTime = Get-Date
         decodePacket $rData $byteData $packetTime
-
-        
+        $packetCount++          
     }
-    
-    
+    Write-Progress -Completed -Activity "Capturing Packets"    
+    Write-Host "Total Packets Captured: $packetCount"    
 }
-function decodePacket{
+Function decodePacket{
         param( [int]$packet,[byte[]]$byteData,[DateTime]$packetTime)
-
-
+        
         $MemoryStream = New-Object System.IO.MemoryStream( $byteData, 0, $packet )
+        $packetSize = $byteData.Length
 	    $BinaryReader = New-Object System.IO.BinaryReader( $MemoryStream )
 
 	    # b1 - version & header length
@@ -204,6 +223,7 @@ function decodePacket{
 			    $sourcePort = NetworkToHostUInt16 $BinaryReader.ReadBytes(2)
 			    $destPort = NetworkToHostUInt16 $BinaryReader.ReadBytes(2)
 			    $serviceDesc = getService( $destPort )
+                if($serviceDesc -eq $null){ $serviceDesc = getService( $sourcePort )}
 			    $SequenceNumber = NetworkToHostUInt32 $BinaryReader.ReadBytes(4)
 			    $AckNumber = NetworkToHostUInt32 $BinaryReader.ReadBytes(4)
 			    $TCPHeaderLength = [int]"0x$(('{0:X}' -f $BinaryReader.ReadByte())[0])" * 4
@@ -239,6 +259,7 @@ function decodePacket{
 			    $sourcePort = NetworkToHostUInt16 $BinaryReader.ReadBytes(2)
 			    $destPort = NetworkToHostUInt16 $BinaryReader.ReadBytes(2)
 			    $serviceDesc = getService( $destPort )
+                if($serviceDesc -eq $null){ $serviceDesc = getService( $sourcePort )}
 			    $UDPLength = NetworkToHostUInt16 $BinaryReader.ReadBytes(2)
 			    [void]$BinaryReader.ReadBytes(2)
 			    # subtract udp header length (2 octets) and convert octets to bytes
@@ -265,7 +286,8 @@ function decodePacket{
 		    $SourceHostName = resolve( $SourceIP )
 	    }
 
-        $properties = @{'Time'=$packetTime;
+        $properties = @{'Packet Number'=$packetNumber;
+                                    'Time'=$packetTime;
                                     'Version'=$ipVersion;
                                     'Protocol'=$ProtocolDesc;
                                     'Destination IP'=$DestinationIP;
@@ -280,7 +302,8 @@ function decodePacket{
                                     'Window'='';
                                     'Flags'='';
                                     'Service'='';
-                                    'Data'=''}
+                                    'Data'='';
+                                    'Data Size'=''}
         $packetInfo = New-Object –TypeName PSObject –Prop $properties
         $packetInfo.PSObject.TypeNames.Insert(0,'Packet')
 
@@ -290,7 +313,7 @@ function decodePacket{
 		{
 			"ICMP"	{
                     $packetInfo.'Type'="$ICMPType - $ICMPTypeDesc"
-                    $packetInfo.'Code'="$ICMPCode - $ICMPCodeDesc"            
+                    $packetInfo.'Code'="$ICMPCode - $ICMPCodeDesc"         
 					break
 				}
 			"IGMP"	{                
@@ -312,29 +335,229 @@ function decodePacket{
 					break
 				}
 		}
-             
+        #$packetSize = $Data.length
+        <#             
 		for( $index = 0; $index -lt $Data.length; $index++ )
 		{
 			# eliminate non ascii characters...
 			if( $Data[$index] -lt 33 -or $Data[$index] -gt 126 )
 			{
+                if($Data[$index] -eq 0){$packetSize--}
 				$Data[$index] = '.'
 			}
-		} 
+		}#>
+        <#
+        for( $index = $byteData.length-1; $index -gt 0; $index-- )
+		{
+			if($byteData[$index] -eq 0){$packetSize--}else{$index = 0}			
+		}#>
+        
+        #if($packetSize -eq 0){$packetSize = $packetInfo.'Window'}
+        $packetInfo.'Data Size' = $packetSize
         $packetInfo.Data = -join [char[]]$Data
-        $decodedPackets.Add($packetInfo)
-	
-		Write-Host "----------------------------------------------------------------------"
+        
+        #$decodedPackets.Add($packetInfo)
+	    analyzePacket $packetInfo
+		
 		#$decodedPackets	    
 }
-
-function analyzePacket{
+Function analyzePacket{
+    param($packet)
     
+    #$packet
+    
+    $destinationIP = $packet.'Destination IP'.IPAddressToString
+    $sourceIP = $packet.'Source IP'.IPAddressToString
+    #Write-Host "$destinationIP - $sourceIP"
+                  
+    if($destinationIP -eq $localIP){
+        $tempIP = $sourceIP
+        $sourceIP = $destinationIP
+        $destinationIP = $tempIP
+    }
+
+    if($packet.'Service' -eq $null -or $packet.'Service' -eq ''){
+        $serviceType = 'Unknown'
+        #Write-Host "--" $log.'Service' "--"
+    }else{
+        $serviceType = $packet.'Service'
+    }
+    if(!$serviceCounts.ContainsKey($serviceType)){
+        $serviceCounts.Add($serviceType,1)
+    }else{
+        $serviceCounts.$serviceType++
+    }
+
+    $protocolType = $packet.Protocol
+    if(!$protocolCounts.ContainsKey($protocolType)){
+        $protocolCounts.Add($protocolType,1)
+    }else{
+        $protocolCounts.$protocolType++
+    }
+
+    if(!$dataCounts.ContainsKey($destinationIP)){
+        $dataCounts.Add($destinationIP,1)
+    }else{
+        $dataCounts.$destinationIP++
+    }
+    
+    $hr = $packet.'Time'.Hour
+    $min = $packet.'Time'.Minute
+    $sec = $packet.'Time'.Second
+    <#
+    if($sec -lt 16){
+        $time = (Get-Date -Hour $hr -Minute $min -Second 0).ToString('hh:mm:ss tt')
+    }elseif($sec -eq 15 -and $sec -lt 31){
+        $time = (Get-Date -Hour $hr -Minute $min -Second 15)
+    }elseif($sec -eq 31 -and $sec -lt 46){
+        $time = (Get-Date -Hour $hr -Minute $min -Second 30)
+    }else{
+        $time = (Get-Date -Hour $hr -Minute $min -Second 45)
+    }#>
+
+    $time = (Get-Date -Hour $hr -Minute $min -Second $sec).ToString('hh:mm:ss')
+    if(!$timeData.ContainsKey($time)){
+        $timeData.Add($time,1)  
+    }else{
+        $timeData.$time++
+    }
+
+}
 
 
+Function buildCharts{
+# create chart object 
+$Script:dataChart = New-object System.Windows.Forms.DataVisualization.Charting.Chart 
+$dataChart.Width = 700 
+$dataChart.Height = 400 
+$dataChart.Left = 40 
+$dataChart.Top = 30
+
+# create a chartarea to draw on and add to chart 
+$dataChartArea = New-Object System.Windows.Forms.DataVisualization.Charting.ChartArea 
+$dataChart.ChartAreas.Add($dataChartArea)
+
+# add data to chart 
+
+$dataChartData = $dataCounts.GetEnumerator() | sort -Property Value -Descending | select -First 3
+
+[void]$dataChart.Titles.Add(“Top 3 Most Active Connections”) 
+$dataChartArea.AxisX.Title = “IP Address” 
+$dataChartArea.AxisY.Title = “Packets”
+
+[void]$dataChart.Series.Add(“Data”) 
+$dataChart.Series[“Data”].Points.DataBindXY($dataChartData.Name, $dataChartData.Value)
+#$dataChart.Series[“Data”].Points.DataBindXY($tempdataChartData.Keys, $tempdataChartData.Values)
+
+# display the chart on a form 
+$dataChart.Anchor = [System.Windows.Forms.AnchorStyles]::Bottom -bor [System.Windows.Forms.AnchorStyles]::Right -bor 
+                [System.Windows.Forms.AnchorStyles]::Top -bor [System.Windows.Forms.AnchorStyles]::Left 
+$dataChart.Series[“Data”].Sort([System.Windows.Forms.DataVisualization.Charting.PointSortOrder]::Descending, “Y”)
+
+$Script:protoChart = New-object System.Windows.Forms.DataVisualization.Charting.Chart 
+$protoChart.Width = 700 
+$protoChart.Height = 400 
+$protoChart.Left = 750 
+$protoChart.Top = 30
+
+# create a chartarea to draw on and add to chart 
+$protoChartArea = New-Object System.Windows.Forms.DataVisualization.Charting.ChartArea 
+$protoChart.ChartAreas.Add($protoChartArea)
+
+# add data to chart 
+$tempprotoChartData = $dataCounts
+$protoChartData = $protocolCounts
+[void]$protoChart.Titles.Add(“Protocols”) 
+[void]$protoChart.Series.Add(“Data”) 
+$protoChart.Series[“Data”].Points.DataBindXY($protoChartData.Keys, $protoChartData.Values)
+#$protoChart.Series[“Data”].Points.DataBindXY($tempprotoChartData.Keys, $tempprotoChartData.Values)
+$protoChart.Series[“Data”].ChartType = [System.Windows.Forms.DataVisualization.Charting.SeriesChartType]::Pie
+# display the chart on a form 
+$protoChart.Anchor = [System.Windows.Forms.AnchorStyles]::Bottom -bor [System.Windows.Forms.AnchorStyles]::Right -bor 
+                [System.Windows.Forms.AnchorStyles]::Top -bor [System.Windows.Forms.AnchorStyles]::Left 
+$protoChart.Series[“Data”].Sort([System.Windows.Forms.DataVisualization.Charting.PointSortOrder]::Descending, “Y”)
+$protoChart.Series[“Data”][“PieLabelStyle”] = “Disabled” 
+($protoChart.Series[“Data”].Points.FindMaxByValue())[“Exploded”] = $true
+#$protoChart.Series[“Data”][“PieLineColor”] = “Black” 
+$legend2 = New-object System.Windows.Forms.DataVisualization.Charting.Legend
+$protoChart.Legends.Add($legend2)
+$Legend2.Name = "Default"
+
+$Script:servChart = New-object System.Windows.Forms.DataVisualization.Charting.Chart 
+$servChart.Width = 700 
+$servChart.Height = 400 
+$servChart.Left = 750 
+$servChart.Top = 440
+
+# create a chartarea to draw on and add to chart 
+$servChartArea = New-Object System.Windows.Forms.DataVisualization.Charting.ChartArea 
+$servChart.ChartAreas.Add($servChartArea)
+
+# add data to chart 
+$tempservChartData = $dataCounts
+$servChartData = $serviceCounts
+[void]$servChart.Titles.Add(“Services”) 
+[void]$servChart.Series.Add(“Data”) 
+$servChart.Series[“Data”].Points.DataBindXY($servChartData.Keys, $servChartData.Values)
+#$servChart.Series[“Data”].Points.DataBindXY($tempservChartData.Keys, $tempservChartData.Values)
+$servChart.Series[“Data”].ChartType = [System.Windows.Forms.DataVisualization.Charting.SeriesChartType]::Pie
+# display the chart on a form 
+$servChart.Anchor = [System.Windows.Forms.AnchorStyles]::Bottom -bor [System.Windows.Forms.AnchorStyles]::Right -bor 
+                [System.Windows.Forms.AnchorStyles]::Top -bor [System.Windows.Forms.AnchorStyles]::Left 
+$servChart.Series[“Data”].Sort([System.Windows.Forms.DataVisualization.Charting.PointSortOrder]::Descending, “Y”)
+$servChart.Series[“Data”][“PieLabelStyle”] = “Disabled” 
+$servChart.Series[“Data”][“Exploded”] = $true
+#$servChart.Series[“Data”][“PieLineColor”] = “Black” 
+$legend = New-object System.Windows.Forms.DataVisualization.Charting.Legend
+$servChart.Legends.Add($legend)
+$Legend.Name = "Default"
+
+$Script:timeChart = New-object System.Windows.Forms.DataVisualization.Charting.Chart 
+$timeChart.Width = 700 
+$timeChart.Height = 400 
+$timeChart.Left = 40 
+$timeChart.Top = 440
+
+# create a chartarea to draw on and add to chart 
+$timeChartArea = New-Object System.Windows.Forms.DataVisualization.Charting.ChartArea 
+$timeChart.ChartAreas.Add($timeChartArea)
+
+# add data to chart 
+$timeChartData = $($timeData.GetEnumerator() | sort -Property name)
+[void]$timeChart.Titles.Add(“Packets over Time”) 
+[void]$timeChart.Series.Add(“Data”) 
+$timeChartArea.AxisX.Title = “Time” 
+$timeChartArea.AxisY.Title = “Packet Counts”
+$timeChart.Series[“Data”].Points.DataBindXY($timeChartData.Name, $timeChartData.Value)
+#$timeChart.Series[“Data”].Points.DataBindXY($temptimeChartData.Keys, $temptimeChartData.Values)
+$timeChart.Series[“Data”].ChartType = [System.Windows.Forms.DataVisualization.Charting.SeriesChartType]::Line
+# display the chart on a form 
+$timeChart.Anchor = [System.Windows.Forms.AnchorStyles]::Bottom -bor [System.Windows.Forms.AnchorStyles]::Right -bor 
+                [System.Windows.Forms.AnchorStyles]::Top -bor [System.Windows.Forms.AnchorStyles]::Left 
+#$timeChart.Series[“Data”].Sort([System.Windows.Forms.DataVisualization.Charting.PointSortOrder]::Ascending, “X”)
+$timeChart.Series[“Data”][“PieLabelStyle”] = “Disabled” 
+$timeChart.Series[“Data”][“Exploded”] = $true
+#$timeChart.Series[“Data”][“PieLineColor”] = “Black” 
 
 
 
 }
+Function displayForm{
+    $Form = New-Object Windows.Forms.Form 
+    $Form.Text = “PowerShell Network Connection Monitor” 
+    $Form.Width = 1500
+    $Form.Height = 930 
+    $Form.controls.add($dataChart) 
+    $Form.controls.add($protoChart) 
+    $Form.controls.add($servChart)
+    $Form.controls.add($timeChart)  
+    $Form.Add_Shown({$Form.Activate()}) 
+    $Form.ShowDialog()
+}
 
-capturePackets "192.168.1.153"
+
+capturePackets $seconds
+ 
+buildCharts
+
+displayForm
